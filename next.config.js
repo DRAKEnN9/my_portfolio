ebce3a712c04ebc2e5b7f0e26f48a10b097b7b2b
DRAKEnN9 <adityadiwakar202@gmail.com>
2025-02-02 16:44:17 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 
 module.exports = {
     reactStrictMode: true,
+    output: "export", // Enable static export
+    images: {
+      unoptimized: true, // Disable image optimization for static export
+    },
     env: {
       NAME: process.env.NAME,
     }
